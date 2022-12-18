ed8d41ecb5746bf55be33f7b3ced6d9260ee93cc
Rohit Chouhan <ro.chouhan@gmail.com>
2022-12-18 16:09:59 -0600

    Add all heroes and factions

---
+++ pages/orderOfRadiantSun.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout"
+import React from "react"
+
+export default function OrderOfRadiantSun() {
+	return (
+		<Layout>
+			<div>Order of Radiant Sun</div>
+		</Layout>
+	)
+}
