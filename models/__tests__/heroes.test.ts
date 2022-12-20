ff4ae41fab6d5e65fb114ea2a9d9e2b0ac9b4838
Rohit Chouhan <ro.chouhan@gmail.com>
2022-12-20 11:48:05 -0600

    Add jest tests

---
+++ models/__tests__/heroes.test.ts
@@ -0,0 +1,35 @@
+import { ALL_HEROES } from "../heroes"
+import { Stats } from "../types"
+
+describe("All heroes", () => {
+	test("should have unique names", () => {
+		const names = ALL_HEROES.map((hero) => hero.name)
+		expect(names).toHaveLength(new Set(names).size)
+	})
+
+	test("should have non-empty descriptions", () => {
+		ALL_HEROES.forEach((hero) => {
+			expect(hero.description).toBeTruthy()
+		})
+	})
+
+	test("average stats should be similar", () => {
+		// Get average stat value for each hero
+		const averageStats = ALL_HEROES.map((hero) => {
+			const { name } = hero
+			const stats = Object.values(hero.stats)
+			const average = stats.reduce((a, b) => a + b) / stats.length
+			return { name, average }
+		})
+
+		// Test that the average is within 1 of the median
+		const median = averageStats.map((a) => a.average).sort()[
+			Math.floor(averageStats.length / 2)
+		]
+
+		averageStats.forEach(({ average }) => {
+			expect(average).toBeGreaterThanOrEqual(median - 1)
+			expect(average).toBeLessThanOrEqual(median + 1)
+		})
+	})
+})
